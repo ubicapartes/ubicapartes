4b29a1641fce46ec5db2a87b2c6becc03daa6721
Winder Ojeda <winder.alejandro@gmail.com>
2017-02-15 08:10:12 -0400

    delete credentials

--- src/main/java/com/okiimport/persistencia/AbstractJpaConfiguration.java
+++ src/main/java/com/okiimport/persistencia/AbstractJpaConfiguration.java
@@ -50,10 +50,9 @@ public class AbstractJpaConfiguration {
     public DataSource dataSource() {
         BasicDataSource dataSource = new BasicDataSource();
         dataSource.setDriverClassName("org.postgresql.Driver");
-        /*dataSource.setUrl("jdbc:postgresql://localhost:5432/requerimientos4");
+        dataSource.setUrl("jdbc:postgresql://localhost:5432/requerimientos4");
         dataSource.setUsername("postgres");
-        dataSource.setPassword("310888");*/
-        dataSource.setUrl("jdbc:postgresql://ec2-54-243-200-110.compute-1.amazonaws.com:5432/d7gsdl0sm1802a?sslmode=require&user=wynvxzxuyzfjcv&password=si5G_o8Tlje0vpWlOzoED0enRA"); 
+        dataSource.setPassword("310888");
 //        dataSource.setTestOnBorrow(Boolean.TRUE);
 //        dataSource.setTestOnReturn(Boolean.TRUE);
 //        dataSource.setTestWhileIdle(Boolean.TRUE);
